a41b570c83693799a5eb5e1443d35b3322ced6a5
unknown <bogdan.lyamzin.d@gmail.com>
2023-12-26 18:39:09 +0200

    finish lesson 11

---
+++ lesson-10/test-function-example/app.js
@@ -0,0 +1,10 @@
+import calcPayment from "./funcs/calcPayment";
+import User from "./funcs/User.js";
+
+calcPayment({});
+
+const newUser = new User("Rogal", "Dorn");
+if(Object.keys(newUser) === 2) {
+
+}
+
